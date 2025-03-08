40e4cb77b6c04d73586186fa1428d17d3cac175d
Tarunchikatipalli <109077273+xTarunkumar06x@users.noreply.github.com>
2025-03-08 13:54:19 -0500

    Updates

---
+++ precache-manifest.fb099fa716db59434521bc9e5626332f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a130dbb17092c5b4ad9c3ee41bb22a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "fa67f9157ce19ba5aa64",
+    "url": "/Portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "8a157a2dd7c4c310d7a1",
+    "url": "/Portfolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "573feaf18ae612e6a49e",
+    "url": "/Portfolio/static/css/main.cdb27ecc.chunk.css"
+  },
+  {
+    "revision": "0cf949c075c0c1b059cd",
+    "url": "/Portfolio/static/js/2.0e190ea8.chunk.js"
+  },
+  {
+    "revision": "d4d701abc30fcef5f714584a64df6e82",
+    "url": "/Portfolio/static/js/2.0e190ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa67f9157ce19ba5aa64",
+    "url": "/Portfolio/static/js/3.936a76bd.chunk.js"
+  },
+  {
+    "revision": "8a157a2dd7c4c310d7a1",
+    "url": "/Portfolio/static/js/4.d0f8f631.chunk.js"
+  },
+  {
+    "revision": "573feaf18ae612e6a49e",
+    "url": "/Portfolio/static/js/main.95fbb151.chunk.js"
+  },
+  {
+    "revision": "0fef36e125b269224157",
+    "url": "/Portfolio/static/js/runtime-main.6c65c6a1.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/Portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/Portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "5bb3bec67252fef5b4adb952c0b6282b",
+    "url": "/Portfolio/static/media/loyolalogo.5bb3bec6.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "6b286424992cc2384fdc82b8f1ec4945",
+    "url": "/Portfolio/static/media/northeasternLogo.6b286424.png"
+  },
+  {
+    "revision": "29b3503964f8b187ca46ebdba1fad66b",
+    "url": "/Portfolio/static/media/profile.29b35039.png"
+  },
+  {
+    "revision": "b8deca710737de3cfe7e23519421c4a8",
+    "url": "/Portfolio/static/media/project1.b8deca71.png"
+  },
+  {
+    "revision": "87a5fa8100eb3ff49109a79d98467fc7",
+    "url": "/Portfolio/static/media/project2.87a5fa81.png"
+  },
+  {
+    "revision": "6a19ead2489715bb9ff7e25462cb0719",
+    "url": "/Portfolio/static/media/project4.6a19ead2.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/Portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "91ccb92e60eb65fe6bce0a9a49386a4c",
+    "url": "/Portfolio/static/media/resume.91ccb92e.pdf"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
